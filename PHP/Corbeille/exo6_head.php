15f542dfd2a1f9dd694e864463f32756b1acf330
SThomas06 <33148230+SThomas06@users.noreply.github.com>
2018-12-18 23:47:11 -0800

    PHP
    addition de la partie PHP

---
+++ PHP/Corbeille/exo6_head.php
@@ -0,0 +1,9 @@
+<?php 
+
+echo '<!DOCTYPE html>
+	<html>
+		<head>
+			<meta charset="utf-8" />
+			<title>EXO 6</title>
+			<h1>HEADER</h1>
+		</head>';
---
+++ PHP/Corbeille/exo7.php
@@ -0,0 +1,9 @@
+<?php
+
+	include("exo6_head.php");
+
+	include("exo6_nav.php");
+
+	include("exo6_footer.php");
+	
+?>
